b8f8f465f28d4b283fcaeca2b3d7cdee6c019c31
anton <asporrer@campus.lmu.de>
2018-02-03 23:31:17 +0100

    1.) Two figures were added. One figure containing two boxplots and one figure containing a plot with multiple y-axis. In addition, a selection drop-down menu was added. 2.) A second page about the influenza project including a navigation bar at the bottom of the two influenza project pages was added. 3.) Email, Xing, Linkedin, Github.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from isoweek import Week
 
 import pandas as pd
@@ -11,7 +11,7 @@ from sklearn.neighbors import KernelDensity
 
 # Plotting
 from bokeh.plotting import figure
-from bokeh.models import DatetimeTickFormatter, ColumnDataSource, LinearColorMapper, HoverTool
+from bokeh.models import Range1d, LinearAxis, DatetimeTickFormatter, ColumnDataSource, LinearColorMapper, HoverTool
 from bokeh.palettes import Category20b
 from bokeh.transform import linear_cmap
 from bokeh.layouts import column, row
@@ -21,16 +21,11 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+# Getting the path to datadf.pkl.
 currDir = os.path.dirname(__file__)
-print(currDir)
-frist_step = os.path.split(currDir)
-print(frist_step)
 data_dir = os.path.join(currDir, "static")
-print(data_dir)
 data_dir = os.path.join(data_dir, "data")
-print(data_dir)
 data_location = os.path.join(data_dir, 'datadf.pkl')
-print(data_location)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd90Fj238A679bn940sn4Ghrq9b08a962Nvfm2390'
@@ -59,22 +54,47 @@ def index():
 
 
 @app.route('/InfluenzaProject', methods=['GET'])
-def render_influenza_project():
+def render_influenza_project1():
     # Generate plots
     p_vanilla_influenza = visualize_state_commonalities(data_df)
     p_overall_reported_cases = visualize_overall_reported_cases(data_df)
     p_wave_stats = visualize_wave_stats_distributions(data_df)
+    p_start_vs_severity = visualize_wave_start_vs_severity_via_box(data_df)
+    p_start_vs_length = visualize_wave_start_vs_length_via_box(data_df)
+    p_wave_features = visualize_data_per_state(data_df)
+
 
     # Embed plots into HTML via Flask Render
     script_vanilla, div_vanilla = components(p_vanilla_influenza)
     script_overall_cases, div_overall_cases = components(p_overall_reported_cases)
     script_wave_stats, div_wave_stats = components(p_wave_stats)
-
+    script_wave_start_vs_intensity, div_wave_start_vs_intensity = components(row(p_start_vs_severity, p_start_vs_length))
+    script_features, div_features = components(p_wave_features)
     state_list = data_df['state'].unique().tolist()
 
     return render_template('InfluenzaProject.html', script_vanilla=script_vanilla, div_vanilla=div_vanilla,
                            script_overall_cases=script_overall_cases, div_overall_cases=div_overall_cases,
-                           script_wave_stats=script_wave_stats, div_wave_stats=div_wave_stats, stateSequence=state_list)
+                           script_wave_stats=script_wave_stats, div_wave_stats=div_wave_stats,
+                           script_wave_start_vs_intensity=script_wave_start_vs_intensity,
+                           div_wave_start_vs_intensity=div_wave_start_vs_intensity,
+                           script_features=script_features,
+                           div_features=div_features,
+                           stateSequence=state_list)
+
+
+@app.route('/InfluenzaProject2', methods=['GET'])
+def render_influenza_project2():
+    # Generate plots
+    p_wave_features = visualize_data_per_state(data_df)
+
+    # Embed plots into HTML via Flask Render
+    script_features, div_features = components(p_wave_features)
+    state_list = data_df['state'].unique().tolist()
+
+    return render_template('InfluenzaProject2.html',
+                           script_features=script_features,
+                           div_features=div_features,
+                           stateSequence=state_list)
 
 
 @app.route('/waveStatisticsFigure')
@@ -90,8 +110,42 @@ def wave_statistics_figure():
     p_wave_stats = visualize_wave_stats_distributions(data_df, states=state_list)
 
     script, div = components(p_wave_stats)
-    print(render_template('waveStatisticsFigure.html', script=script, div=div))
-    return render_template('waveStatisticsFigure.html', script=script, div=div)
+
+    return render_template('multiselectUpdatedFigure.html', script=script, div=div)
+
+
+@app.route('/waveStartVsIntensityFigure')
+def wave_start_vs_intensity_figure():
+    """
+    This function is called via an ajax. The code is located in ajaxscripts.js.
+
+    :return: A str, containing the respective html and javascript code.
+    """
+
+    state_list = request.args.getlist('jsdata[]')
+    p_start_vs_severity = visualize_wave_start_vs_severity_via_box(data_df, states=state_list)
+    p_start_vs_length = visualize_wave_start_vs_length_via_box(data_df, states=state_list)
+
+    script, div = components(row(p_start_vs_severity, p_start_vs_length))
+
+    return render_template('multiselectUpdatedFigure.html', script=script, div=div)
+
+
+@app.route('/featuresFigure')
+def features_figure():
+    """
+    This function is called via an ajax. The code is located in ajaxscripts.js.
+
+    :return: A str, containing the respective html and javascript code.
+    """
+
+    state_str = request.args['jsdata']
+
+    p_wave_features = visualize_data_per_state(data_df, state_str=state_str)
+
+    script, div = components(p_wave_features)
+
+    return render_template('multiselectUpdatedFigure.html', script=script, div=div)
 
 
 #######################
@@ -405,6 +459,183 @@ def kde_sklearn(x, x_grid, bandwidth=0.2, **kwargs):
     return np.exp(log_pdf)
 
 
+def visualize_wave_start_vs_severity_via_box(input_df, states=['all']):
+
+    first_last_max_length_lists = list(get_first_last_week_max_length_of_wave(input_df, current_states=states).items())
+    first_list = first_last_max_length_lists[0][1]
+
+    week_list = ["Week " + str(year_week[1]) for year_week in first_list]
+    max_value_of_wave_list = first_last_max_length_lists[2][1]
+
+    week_categories_unique_list = sorted(list(set(week_list)))
+    week_categories_display_order_list = sorted(week_categories_unique_list, key=lambda x: int(x[-2:].strip()))
+
+    return box_plot(week_list, max_value_of_wave_list, week_categories_display_order_list, title="Wave Start vs Wave Severity", x_axis_label="Calender Week", y_axis_label='Number of Infected per 100 000 Inhabitants')
+
+
+def visualize_wave_start_vs_length_via_box(input_df, states=['all']):
+    first_last_max_length_lists = list(get_first_last_week_max_length_of_wave(input_df, current_states=states).items())
+
+    start_length_count_df = get_first_length_count_df(first_last_max_length_lists[0][1],
+                                                      first_last_max_length_lists[3][1])
+
+    x_count_list = ["Week " + str(week) for week in start_length_count_df['first_week'].tolist()]
+    y_count_list = start_length_count_df['wave_length'].tolist()
+    count_for_size_list = start_length_count_df['count'].tolist()
+
+    first_week_list = ["Week " + str(year_week[1]) for year_week in first_last_max_length_lists[0][1]]
+    wave_length_list = first_last_max_length_lists[3][1]
+
+    week_categories_unique_list = sorted(list(set(first_week_list)))
+    week_categories_display_order_list = sorted(week_categories_unique_list, key=lambda x: int(x[-2:].strip()))
+
+    box_fig = box_plot(first_week_list, wave_length_list, week_categories_display_order_list, title="Wave Start vs Wave Length in Weeks", x_axis_label="Calender Week", y_axis_label="Calender Week")
+    # Encoding the count of the start week, wave length pair in the x size.
+    box_fig.x(x_count_list, y_count_list, color='black', size=np.array(count_for_size_list)*3)
+
+    return box_fig
+
+
+def box_plot(x_list, y_list, x_cat_unique_display_order_list, title="", x_axis_label="", y_axis_label=""):
+
+    x_categories_unique_sorted_list = sorted(list(set(x_list)))
+    first_max_df = pd.DataFrame(columns=['group', 'score'])
+    first_max_df[first_max_df.columns[0]] = x_list
+    first_max_df[first_max_df.columns[1]] = y_list
+
+    # Find the quartiles and IQR for each category
+    groups = first_max_df.groupby('group')
+    q1 = groups.quantile(q=0.25)
+    q2 = groups.quantile(q=0.5)
+    q3 = groups.quantile(q=0.75)
+    iqr = q3 - q1
+    upper = q3 + 1.5 * iqr
+    lower = q1 - 1.5 * iqr
+
+    # Find the outliers for each category
+    def outliers(group):
+        cat = group.name
+        return group[(group.score > upper.loc[cat]['score']) | (group.score < lower.loc[cat]['score'])]['score']
+
+    out = groups.apply(outliers).dropna()
+
+    # Prepare outlier data for plotting, we need coordinates for every outlier.
+    if not out.empty:
+        outx = []
+        outy = []
+        for cat in x_categories_unique_sorted_list:
+            # Only add outliers if they exist
+            if not out.loc[cat].empty:
+                for value in out[cat]:
+                    outx.append(cat)
+                    outy.append(value)
+
+    p = figure(title=title, plot_width=400, plot_height=350, x_range=x_cat_unique_display_order_list, x_axis_label=x_axis_label,
+               y_axis_label=y_axis_label)
+    # if no outliers, shrink lengths of stems to be no longer than the minimums or maximums
+    qmin = groups.quantile(q=0.00)
+    qmax = groups.quantile(q=1.00)
+    upper.score = [min([x, y]) for (x, y) in zip(list(qmax.loc[:, 'score']), upper.score)]
+    lower.score = [max([x, y]) for (x, y) in zip(list(qmin.loc[:, 'score']), lower.score)]
+
+    # stems
+    p.segment(x_categories_unique_sorted_list, upper.score, x_categories_unique_sorted_list, q3.score, line_color="black")
+    p.segment(x_categories_unique_sorted_list, lower.score, x_categories_unique_sorted_list, q1.score, line_color="black")
+
+    # boxes
+    p.vbar(x_categories_unique_sorted_list, 0.7, q2.score, q3.score, fill_color="#E08E79", line_color="black")
+    p.vbar(x_categories_unique_sorted_list, 0.7, q1.score, q2.score, fill_color="#3B8686", line_color="black")
+
+    # whiskers (almost-0 height rects simpler than segments)
+    p.rect(x_categories_unique_sorted_list, lower.score, 0.2, 0.01, line_color="black")
+    p.rect(x_categories_unique_sorted_list, upper.score, 0.2, 0.01, line_color="black")
+
+    # outliers
+    if not out.empty:
+        p.circle(outx, outy, size=6, color="#F38630", fill_alpha=0.6)
+
+    p.x(x_list, y_list, color='black')
+
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = "white"
+    p.grid.grid_line_width = 2
+    p.xaxis.major_label_text_font_size = "12pt"
+    p.xaxis.major_label_orientation = 3.0 / 4
+
+    return p
+
+
+def get_first_length_count_df(first_list, length_list):
+    count_dict = defaultdict(lambda: 0)
+    for index in range(len(first_list)):
+        count_dict[(first_list[index][1], length_list[index])] += 1
+    return pd.DataFrame([[item[0][0], item[0][1], item[1]] for item in count_dict.items()], columns=['first_week',
+                                                                                                     'wave_length', 'count'])
+
+
+def visualize_data_per_state(input_df, state_str="Baden-Wuerttemberg"):
+
+    # State information
+    state_indices = input_df['state'] == state_str
+    state_df = input_df[state_indices]
+
+    influenza_list = state_df['influenza_week-1'].tolist()[:-1]
+    influenza_germany_list = state_df['influenza_germany_week-1'].tolist()[:-1]
+    google_trends_list = state_df['trend_week-1'].tolist()[:-1]
+    google_trends_germany_list = state_df['trend_germany_week-1'][:-1]
+    temp_list = state_df['temp_mean-1'].div(10).tolist()[:-1]
+    # humid_list = state_df['humid_mean-1'].tolist()[:-1]
+    # prec_list = state_df['prec_mean-1'].tolist()[:-1]
+    dates_list = state_df['year_week'].unique().tolist()[1:]
+
+    p = figure(plot_width=800, plot_height=500, title=state_str, x_axis_label='Date')
+
+    plot_x = [Week(year_week[0], year_week[1]).monday() for year_week in
+                  dates_list] # Old can be deleted: datetime.strptime(str(year_week[0]) + 'W' + str(year_week[1]) + ' MON', '%YW%U %a')
+
+    p.xaxis.formatter = DatetimeTickFormatter(
+        years=["%D %B %Y"]
+    )
+
+    p.xaxis.major_label_orientation = 3.0 / 4
+    p.xaxis[0].ticker.desired_num_ticks = 30
+
+    # Influenza Numbers for Current State and for Germany as a Whole
+    p.yaxis.axis_label = '#Influenza Infections'
+    p.y_range = Range1d(start=0, end=max(max(influenza_list), max(influenza_germany_list))+3)
+
+    # Google Trends Data
+    p.extra_y_ranges['trends'] = Range1d(start=0, end=max(max(google_trends_list), max(google_trends_germany_list)))
+    p.add_layout(LinearAxis(y_range_name='trends', axis_label='Trends in'), 'left')
+
+    # Temperature
+    p.extra_y_ranges['temp'] = Range1d(start=min(temp_list)-1, end=max(temp_list)+2)
+    p.add_layout(LinearAxis(y_range_name='temp', axis_label='Temperature in Degree Celsius'), 'right')
+
+    # # Precipitation
+    # p.extra_y_ranges['prec'] = Range1d(start=0, end=500)
+    # p.add_layout(LinearAxis(y_range_name='prec', axis_label='Prec in'), 'right')
+
+    keys_list = ['Influenza', 'Influenza Germany', 'Google Trends', 'Google Trends Germany', 'Temperature']
+    argument_list = [influenza_list, influenza_germany_list, google_trends_list, google_trends_germany_list, temp_list]
+    color_list = ['black', 'blue', 'green', 'navy', 'red']
+    y_range_list = ['dummy', 'dummy', 'trends', 'trends', 'temp']
+
+    for index in range(0, 2):
+        p.step(plot_x, argument_list[index], legend=keys_list[index], color=color_list[index], alpha=1.0,
+               muted_color=color_list[index], muted_alpha=0.0)
+
+    for index in range(2, 5):
+        p.step(plot_x, argument_list[index], legend=keys_list[index], color=color_list[index], alpha=1.0,
+               muted_color=color_list[index], muted_alpha=0.0, y_range_name=y_range_list[index])
+
+    p.legend.location = "top_left"
+    p.legend.click_policy = "mute"
+    p.legend.background_fill_alpha = 0.0
+
+    return p
+
+
 if __name__ == "__main__":
-    # app.run(debug=True)
-    app.run(port=33507)
+     app.run(debug=True)
+    # app.run(port=33507)
--- static/js/ajaxscripts.js
+++ static/js/ajaxscripts.js
@@ -1,12 +1,44 @@
 $("#confirmMultiselectWaveStatistics").mouseup(function(){
-    var text2 = $("#waveStatisticsMultiselect").val();
+    var text = $("#waveStatisticsMultiselect").val();
 
     $.ajax({
       url: "/waveStatisticsFigure",
       type: "get",
-      data: {jsdata: text2},
+      data: {jsdata: text},
       success: function(response) {
         $("#placeForWaveStatisticsFigure").html(response);
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+      }
+    });
+});
+
+$("#confirmMultiselectWaveStartVsInensityStatistics").mouseup(function(){
+    var text = $("#waveStartVsIntensityMultiselect").val();
+
+    $.ajax({
+      url: "/waveStartVsIntensityFigure",
+      type: "get",
+      data: {jsdata: text},
+      success: function(response) {
+        $("#placeForWaveStartVsIntensityFigure").html(response);
+      },
+      error: function(xhr) {
+        //Do Something to handle error
+      }
+    });
+});
+
+$("#featuresSingleSelect").change(function(){
+    var text = $("#featuresSingleSelect").val();
+
+    $.ajax({
+      url: "/featuresFigure",
+      type: "get",
+      data: {jsdata: text},
+      success: function(response) {
+        $("#placeForFeaturesFigure").html(response);
       },
       error: function(xhr) {
         //Do Something to handle error
